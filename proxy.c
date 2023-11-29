cb5715fbcc987c5bdf0315e1453c131d242d9ab7
Francisco Sanchez <fsanchez@goodleap.com>
2023-11-29 10:10:34 -0800

    Working headers

--- proxy.c
+++ proxy.c
@@ -7,7 +7,7 @@
 #define MAX_OBJECT_SIZE 102400
 
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *user_agent_hdr = "Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3";
 
 int main(int argc, char **argv)
 {
@@ -47,6 +47,7 @@ void send_headers(int fd, HashMap *headers)
         Pair *pair = headers->list[i];
         while (pair)
         {
+            buf[0] = '\0'; // reset buf
             sprintf(buf, "%s: %s\r\n", pair->key, pair->val);
             printf("%s", buf);
             Rio_writen(fd, buf, strlen(buf));
